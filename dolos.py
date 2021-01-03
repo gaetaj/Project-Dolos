767e7acf8186e31d4a88a87231267fb0ab7207f5
Noah Gaeta <ngaet02@gmail.com>
2021-01-03 17:29:34 -0500

    Added way to get padding

--- dolos.py
+++ dolos.py
@@ -1,12 +1,13 @@
 import argparse
 from os import path
-
+from src.linux.find_padding import find_padding
 
 def main():
     parser = argparse.ArgumentParser(description='Steganography Tool to Identify Binary/Project Leakers')
     parser.add_argument('binary_path', help='Path to leaked binary')
     args = parser.parse_args()
     assert_valid_arguments(args)
+    find_padding(args.binary_path)
     return 0
 
 
