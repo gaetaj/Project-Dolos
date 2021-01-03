40539b0a58c53a2746bcf68400e9a50d2db6e3c4
Noah Gaeta <ngaet02@gmail.com>
2021-01-03 16:42:25 -0500

    Added basic cmd portion of tool

---
+++ dolos.py
@@ -0,0 +1,18 @@
+import argparse
+from os import path
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Steganography Tool to Identify Binary/Project Leakers')
+    parser.add_argument('binary_path', help='Path to leaked binary')
+    args = parser.parse_args()
+    assert_valid_arguments(args)
+    return 0
+
+
+def assert_valid_arguments(args):
+    assert path.exists(args.binary_path), 'Path provided does not exist'
+
+
+if __name__ == '__main__':
+    main()
